dc3389d80e71ae717ccebf494d6cb34a7540a5c1
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-10-11 20:04:29 +0200

    Add documentation

--- CppStores.h
+++ CppStores.h
@@ -5,6 +5,10 @@
 #include <functional>
 #include <cstdint>
 
+/**
+ * \brief A writable store for reactive data storage and manipulation
+ * \tparam T The type of the held type
+ */
 template<typename T>
 class Writable {
 private:
@@ -17,24 +21,45 @@ private:
         }
     }
 public:
+    /**
+     * \brief Creates a writable store with a default value
+     * \param value The default value
+     */
     Writable(T value) {
         this->value = value;
     }
 
+    /**
+     * \brief Updates the store's value and notifies subscribers
+     * \param callback A callback which receives the store's value and returns a mutated value
+     */
     void update(std::function<T(T)> callback) {
         this->value = callback(this->value);
         on_value_changed();
     }
 
+    /**
+     * \brief Sets the store's value and notifies all subscribers
+     * \param value The store's value
+     */
     void set(T value) {
         this->value = value;
         on_value_changed();
     }
 
+    /**
+     * \return The store's internal value
+     * \remark By using this, no reactivity is gained 
+     */
     T immediate_value() {
         return this->value;
     }
 
+    /**
+     * \brief Subscribes to value changes
+     * \param callback The callback to be invoked upon a value change
+     * \return An action to be invoked by the caller upon unsubscribing
+     */
     std::function<void()> subscribe(std::function<void(T)> callback) {
         this->subscribers.push_back(std::make_pair(this->current_subscriber_index, callback));
         this->current_subscriber_index++;
